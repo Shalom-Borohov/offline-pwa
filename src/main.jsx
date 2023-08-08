34b1dc784e4da42e1eac9f50ce32ab04d47da2aa
Shalom Borohov <shalom842633@gmail.com>
2023-08-08 12:05:56 +0300

    Add service worker

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+if (navigator.serviceWorker.controller) {
+  console.log("Active service worker found");
+} else {
+  navigator.serviceWorker
+    .register("serviceWorker.js", {
+      scope: "./",
+    })
+    .then(function () {
+      console.log("Service worker  registered");
+    });
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
